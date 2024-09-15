a96007bbee50e4cf319e2e329d3d3d5f5ca9fee0
Daniel Margavio <danmargavio@gmail.com>
2024-09-15 09:09:44 -0500

    improvements

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,10 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.ExampleThing;
 
+/**
+ * This is the main Robot Container class that contains all major robot subsystems
+ * 
+ */
 public class RobotContainer {
   private final CommandXboxController driverJoystick = new CommandXboxController(0); 
 
@@ -16,9 +20,17 @@ public class RobotContainer {
   public static final ExampleThing m_example2 = new ExampleThing(2);
   public static final Arm m_arm = new Arm();
 
+  /**
+   * This is the Robot Container class constructor
+   * 
+   */
   public RobotContainer() {
   }
 
+  /**
+   * This is a method for periodically polling controllers. Note that this isn't meant to be used for normal controls using commands, but for testing purposes
+   * 
+   */
   public void runManualCommands() {
     if (driverJoystick.rightBumper().getAsBoolean()) {
       m_arm.setArmPosition(3.5); 
--- src/main/java/frc/robot/subsystems/ExampleThing.java
+++ src/main/java/frc/robot/subsystems/ExampleThing.java
@@ -6,18 +6,35 @@ public class ExampleThing extends SubsystemBase{
 
     // Unique fields get added here, such as motor objects
 
+    /**
+     * Constructs an "ExampleThing" object with an input parameter "i"
+     * 
+     * @param i This is just an example input parameter that is copied into a member variable "ID"
+     */
     public ExampleThing(int i) {
-        // Construction stuff goes here...
         int ID = i;
     }
+    /**
+     * This is a method template for changing the state of the subsystem
+     * 
+     */
     public void setState() {
         // Tell the thing to do the thing!
     }
+    /**
+     * This is a method template for querying the state of the subsystem
+     * 
+     * @return This is just an example output that is a double
+     */
     public double getState() {
         // Ask the thing whats going on!!
-        return 0;
+        return 0.0;
     }
 
+    /**
+     * This is a method template for the periodic function
+     * 
+     */
     @Override  // This override is important because we are extending a base class with this method!
     public void periodic() {
         // Put all the code you want to run periodically here...
